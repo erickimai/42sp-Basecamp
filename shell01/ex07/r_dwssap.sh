d939f2801a5a134787beee06c132368d206cf0b9
Erick <erick.imai.rodrigues@gmail.com>
2022-04-03 05:54:29 +0200

    Add solution to ex07/ex08

--- shell01/ex07/r_dwssap.sh
+++ shell01/ex07/r_dwssap.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-cat /etc/passwd | cut -d':' -f1 | awk 'NR%2==0' | rev | sort -r | 
+cat /etc/passwd | cut -d':' -f1 | awk 'NR%2==0' | rev | sort -r | sed -n "/$FT_LINE1/,/$FT_LINE2/p"| sed ':a;N;s/\n/, /;ba' | tr '\n' '.'
