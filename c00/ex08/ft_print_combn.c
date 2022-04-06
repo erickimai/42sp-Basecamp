29d84e8d2056c6ea3499d6104b27dfdbce419384
Erick <erick.imai.rodrigues@gmail.com>
2022-04-06 23:46:06 +0200

    Add solution to ex08

--- c00/ex08/ft_print_combn.c
+++ c00/ex08/ft_print_combn.c
@@ -6,22 +6,74 @@
 /*   By: erhenriq <erhenriq@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/04/04 23:36:02 by erhenriq          #+#    #+#             */
-/*   Updated: 2022/04/05 03:20:27 by erhenriq         ###   ########.fr       */
+/*   Updated: 2022/04/06 23:45:29 by erhenriq         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <unistd.h>
 
+void	ft_buildArray(int n);
+void	ft_writeArray(int array[], int n, int endWrite);
+void	ft_iterateArray(int n, int array[]);
+
+void	ft_iterateArray(int n, int array[])
+{
+	int	iterator;
+	int endWrite;
+
+	endWrite = 0;
+	iterator = n - 1;
+	while (iterator >= 0)
+	{
+		if (array[iterator] < array[iterator + 1] - 1)
+		{
+			array[iterator]++;
+				if(iterator != n - 1)
+					array[iterator + 1] = array[iterator] + 1;
+			iterator = n - 1;
+			if (array[0] == 10 - n)
+				endWrite = 1;
+			ft_writeArray(array, n, endWrite);
+		}
+		else
+			iterator--;
+	}
+}
+
+void	ft_buildArray(int n)
+{
+	int	i;
+	int	array[n];
+
+	array[0] = 0;
+	array[n] = 10;
+	i = 1;
+	while (i < n)
+	{
+		array[i] = array[i - 1] + 1;
+		i++;
+	}
+	ft_writeArray(array, n, 0);
+	ft_iterateArray(n, array);
+}
+
+void	ft_writeArray(int array[], int n, int endWrite)
+{
+	int	i;
+	char convertToChar;
+
+	i = 0;
+	while (i < n)
+	{
+		convertToChar = array[i] + 48;
+		write(1, &convertToChar, 1);
+		i++;
+	}
+	if (endWrite == 0)
+		write(1, " ", 1);
+}
+
 void	ft_print_combn(int n)
 {
-	int	nbr[n];
-	int i;
-
-	i = n;
-	while (i > 0)
-	{
-		nbr[i - 1] = '0';
-		write(1, &nbr[i - 1], 1);
-		i--;
-	}
+	ft_buildArray(n);
 }
---
+++ c00/ex08/test.c
@@ -0,0 +1,13 @@
+#include <unistd.h>
+#include <stdio.h>
+
+void ft_print_combn(int n);
+
+int	main (void)
+{
+	int	n;
+
+	n = 2;
+	ft_print_combn(n);
+	return (0);
+}
